194e58fc3f0b50a6dfb0ed0d38595477ba8019cd
Diogo Mendes <diogoafm6@gmail.com>
2023-11-16 01:08:11 +0000

    organize the code

--- src/pages/loja.js
+++ src/pages/loja.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from './loja.css'
+import Link from 'next/link'
 
 const loja = () => {
 
@@ -9,6 +10,11 @@ const loja = () => {
     const [zip,setZip] = useState()
     const [numero, setNumero] = useState()
     const [artigo, setArtigo] = useState()
+    const [entrega, setEntrega] = useState(false)
+    const [preco_entrega, setPreco_entrega] = useState();
+    const [montagem, setMontagem] = useState(false);
+    const [preco_montagem, setPreco_montagem] = useState();
+
 
     const [numberOfContaineres, setNumberOfContaineres] = useState([])
 
@@ -23,37 +29,62 @@ const loja = () => {
         ]);
     };
 
+    const display_values = () => {
+        alert(nome);
+        alert(morada);
+        alert(zip);
+        alert(numero);
+        alert(entrega)
+    }
+
+    const submit_values = (e) => {
+        e.preventDefault();
+        console.log(e)
+    }
+
+    const handleCheckboxChange_1 = () => {
+        setEntrega(!entrega); // Toggle the checkbox state
+      };
+
+    const handleCheckboxChange_2 = () => {
+        setMontagem(!montagem); // Toggle the checkbox state
+    };
+
   return (
     <div className='entregas_body'>
+        <div>
+            <p><Link href="/loja">Loja</Link></p>
+            <p><Link href="/transportes">Entregas</Link></p>
+        </div>
         <div> 
             <div>
                 <h1>Marcaçoes de entregas</h1>
             </div>
             <div>
-                <form>
+                <form onSubmit={submit_values}>
                     <div>
                         <label className='input-styles' >
                             Nome:
                         </label>
-                        <input type="text" name="query" size="50" />
+                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} name="nome_input" size="50" />
                     </div>
                     <div>
                         <label className='input-styles' >
                             Morada:
                         </label>
-                        <input type="text" name="query" size="50"/>
+                        <input type="text" onChange={(e) => setMorada(e.target.value)} name="morada_input" size="50"/>
                     </div>
                     <div>
                         <label className='input-styles' >
                             Codigo de Postal:
                         </label>
-                        <input type="text" name="query" size="50"/>
+                        <input type="text" onChange={(e) => setZip(e.target.value)} name="zip_code_input" size="50"/>
                     </div>
                     <div>
                         <label className='input-styles' >
                             Numero:
                         </label>
-                        <input type="text" name="query" size="50"/>
+                        <input type="text" onChange={(e) => setNumero(e.target.value)} name="numero_input" size="50"/>
                     </div>
                     <h2>Serviço:</h2>
                     <div className='input-serviço'>
@@ -61,13 +92,17 @@ const loja = () => {
                             <label className='input-styles-servico'>
                                 Entrega:
                             </label>
-                            <input type="checkbox" name="entrega" size="50"/>
+                            <input type="checkbox" onChange={handleCheckboxChange_1} name="entrega" size="50"/>
+                            <>
+                                <p className='montar_styles'> - preço:</p>
+                                <input step="1" onChange={(e) => setPreco_entrega(e.target.value)} type="number" min="0" max="100"/>
+                            </>
                         </div>
                         <div className='input-styles'>
                             <label className='input-styles-servico'>
                                 Montagem:
                             </label>
-                            <input type="checkbox" name="montagem"/>
+                            <input type="checkbox" onChange={handleCheckboxChange_2} name="montagem"/>
                         </div>
                     </div>
                     <div>
@@ -81,8 +116,10 @@ const loja = () => {
                                 <div className='button_styles_montar'>
                                     <input type="checkbox" size="50"/>
                                 </div>
-                                <p className='montar_styles'>preço:</p>
-                                <input step="1" type="number" min="0" max="100"/>
+                                <>
+                                    <p className='montar_styles'>preço:</p>
+                                    <input  step="1" type="number" min="0" max="100"/>
+                                </>
                             </div>
                         </div>
                     </div>
@@ -90,8 +127,9 @@ const loja = () => {
                         <h2>Observações:</h2>
                         <textarea name="postContent" rows={6} cols={60} />
                     </div>
+                    <button onClick={() => alert(nome)}>Submit</button>
                 </form>
-                <button onClick={() => addArtigo()}>mais artigo</button>
+                <button onClick={() => display_values()}>mais artigo</button>
             </div>
         </div>
     </div>
--- src/pages/transportes.js
+++ src/pages/transportes.js
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const transportes = () => {
   return (
-    <div>transportes</div>
+    <div>
+      <div>
+            <p><Link href="/loja">Loja</Link></p>
+            <p><Link href="/transportes">Entregas</Link></p>
+      </div>
+      <div>transportes</div>
+    </div>
   )
 }
 
